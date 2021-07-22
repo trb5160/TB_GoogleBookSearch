bfd01e653478d25339565e504bb5e25a952a404c
Tom Barber <trb5160@gmail.com>
2021-07-21 20:52:49 -0400

    updated code for heroku deployment

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/GoogleBooks";
 
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
